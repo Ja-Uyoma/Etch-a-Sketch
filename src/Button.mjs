b8ae739c6163999b2c61f6b49fa0e4d2300004d7
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-21 18:23:01 +0300

    feat: render new grid from dimensions provided in prompt

--- src/Button.mjs
+++ src/Button.mjs
@@ -1,21 +1,30 @@
+import { CreateGrid, RenderGrid } from "./Grid.mjs";
+
 /**
  * Set the number of cells of the new grid to be rendered to the DOM
  * @returns Whether the button was clicked and the number of grid cells
  */
 const HandleButtonClicked = () => {
   let dimensions = 0;
-  let clicked = false;
 
   const btn = document.querySelector("main > button");
   btn.addEventListener("click", () => {
     dimensions = Number(prompt("Enter the grid dimensions"));
-    clicked = true;
-  });
 
-  return {
-    buttonClicked: clicked,
-    dimensions: dimensions > 100 ? 100 : dimensions,
-  };
+    if (dimensions > 100) {
+      dimensions = 100;
+    } else if (dimensions < 0) {
+      dimensions = 10;
+    }
+
+    let cells = document.querySelectorAll("div.container > div");
+    cells.forEach((cell) => {
+      cell.remove();
+    });
+
+    let grid = CreateGrid(dimensions, dimensions);
+    RenderGrid(grid);
+  });
 };
 
 export default HandleButtonClicked;
