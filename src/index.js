c879c5645f88ca2010196b756103db17fa65e1f6
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-21 17:13:28 +0300

    refactor: use functions defined in modules
    - Replace the functions previously defined in src/index.js with
    those defined in the UpdateYear and Grid modules
    - Update the script type of src/index.js in index.html

--- src/index.js
+++ src/index.js
@@ -1,50 +1,11 @@
-/**
- * Create an n x n grid of divs
- * @param {number} gridRows The number of rows of the grid
- * @param {number} gridColumns The number of columns of the grid
- * @returns An array containing the grid cells
- */
-function createGridCells(gridRows, gridColumns) {
-  let array = [];
+import { CreateGrid, RenderGrid, ChangeGridCellBgColourOnMouseOver } from "./Grid.mjs";
+import UpdateYear from "./UpdateYear.mjs";
 
-  for (let i = 0; i < gridRows * gridColumns; ++i) {
-    array.push(document.createElement("div"));
-  }
+const main = () => {
+  UpdateYear();
+  let grid = CreateGrid(4, 4);
+  ChangeGridCellBgColourOnMouseOver(grid, "red");
+  RenderGrid(grid);
+};
 
-  return array;
-}
-
-/**
- * Render the grid cells to the viewport
- */
-function renderGrid() {
-  const container = document.querySelector("div.container");
-
-  let array = createGridCells(4, 4);
-  array.forEach((cell) => {
-    container.appendChild(cell);
-  });
-
-  changeGridCellColourOnMouseover(array, "red");
-  updateYearDynamically();
-}
-
-/**
- * Change the colour of a cell when a mouse hovers over it
- * @param {HTMLElement[]} gridCells Array of grid cells
- * @param {string} colour The new background colour of the grid cell
- */
-function changeGridCellColourOnMouseover(gridCells, colour) {
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = colour;
-    });
-  });
-}
-
-function updateYearDynamically() {
-  const year = new Date().getFullYear().toString();
-  document.querySelector("span.year").textContent = year;
-}
-
-renderGrid();
+main();
