664264ed1d885d1ad00dc11b8ed510baf57e1760
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-21 17:56:41 +0300

    feat: use a prompt to set the grid dimensions on button clicked

--- src/Button.mjs
+++ src/Button.mjs
@@ -1,8 +1,12 @@
 const HandleButtonClicked = () => {
+  let dimensions = 0;
+
   const btn = document.querySelector("main > button");
   btn.addEventListener("click", () => {
-    alert("Button clicked!");
+    dimensions = Number(prompt("Enter the grid dimensions"));
   });
+
+  return dimensions;
 };
 
 export default HandleButtonClicked;
