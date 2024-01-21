e07df75dce3a785a2b9820ed74559e738b8eb95e
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-21 16:54:43 +0300

    feat: add module for grid creation and manipulation
    - Add function CreateGrid
    - Add function RenderGrid
    - Add function ChangeGridCellBgColourOnMouseOver

---
+++ src/Grid.mjs
@@ -0,0 +1,42 @@
+/**
+ * Create an n x n grid of divs
+ * @param {number} gridRows The number of rows of the grid
+ * @param {number} gridColumns The number of columns of the grid
+ * @returns An array containing the grid cells
+ */
+const CreateGrid = (gridRows, gridColumns) => {
+  let array = [];
+
+  for (let i = 0; i < gridRows * gridColumns; ++i) {
+    array.push(document.createElement("div"));
+  }
+
+  return array;
+};
+
+/**
+ * Render the grid cells to the DOM
+ * @param {HTMLElement[]} gridCells An array of divs representing the grid cells
+ */
+const RenderGrid = (gridCells) => {
+  const container = document.querySelector("div.container");
+
+  gridCells.forEach((cell) => {
+    container.appendChild(cell);
+  });
+};
+
+/**
+ * Change the colour of a cell when a mouse hovers over it
+ * @param {HTMLElement[]} gridCells An array of divs representing the grid cells
+ * @param {string} colour The new background colour of the grid cells
+ */
+const ChangeGridCellBgColourOnMouseOver = (gridCells, colour) => {
+  gridCells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = colour;
+    });
+  });
+};
+
+export default { CreateGrid, RenderGrid, ChangeGridCellBgColourOnMouseOver };
