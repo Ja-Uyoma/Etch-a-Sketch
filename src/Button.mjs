b0b231773e81e64c939e05e01a543c93df3138b4
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-21 18:08:01 +0300

    feat: return whether the button was clicked and the grid dimensions

--- src/Button.mjs
+++ src/Button.mjs
@@ -1,16 +1,21 @@
 /**
  * Set the number of cells of the new grid to be rendered to the DOM
- * @returns The number of grid cells
+ * @returns Whether the button was clicked and the number of grid cells
  */
 const HandleButtonClicked = () => {
   let dimensions = 0;
+  let clicked = false;
 
   const btn = document.querySelector("main > button");
   btn.addEventListener("click", () => {
     dimensions = Number(prompt("Enter the grid dimensions"));
+    clicked = true;
   });
 
-  return dimensions > 100 ? 100 : dimensions;
+  return {
+    buttonClicked: clicked,
+    dimensions: dimensions > 100 ? 100 : dimensions,
+  };
 };
 
 export default HandleButtonClicked;
